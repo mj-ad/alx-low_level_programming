2015b60b87451c024fe988590b5e111d2a4ee76b
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2022-09-13 07:10:02 -0700

    Header file for 0x02-functions_nested_loops"

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *File: "main.h"
+ *Auth: "Mojolajah Adekunle"
+ *Desc: "This header file contains all the description for the functions used in 0x02-functions_nested_loops"
+ */
+
+int _putchar(char c);
+#endif
