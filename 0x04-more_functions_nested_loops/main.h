657dcbe51eeb748d3ecd29297d49114ec837e04e
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2022-09-15 04:56:16 -0700

    This header file contains all the description for the functions used in 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *File: "main.h"
+ *Auth: "Mojolajah Adekunle"
+ *Desc: "This header file contains all the description for the
+ *       functions used in 0x04-more_functions_nested_loops"
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+#endif
