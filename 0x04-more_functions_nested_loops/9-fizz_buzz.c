f34171276520ee16d0bd5f857205e44d436d3453
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2022-09-15 10:19:17 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 int c;
 for (c = 1; c <= 100; c++)
 {
-if (c % 5 == 0 && c % 3 == 0)
+ if ((c % 5) == 0 && (c % 3) == 0)
 {
 printf("FizzBuzz");
 }
-else if (c % 3 == 0)
+else if ((c % 3) == 0)
 {
 printf("Fizz");
 }
-else if (c % 5 == 0)
+else if ((c % 5) == 0)
 {
 printf("Buzz");
 }
@@ -33,4 +33,5 @@ continue;
 }
 printf(" ");
 }
+return (0);
 }
