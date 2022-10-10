ea235f2f0fc829bf4b6f2d95b0d68b72c46f07ce
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2022-10-10 05:59:33 -0700

    Create a header file that defines a macro named SIZE as an abbreviation for the token 1024.

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,10 @@
-#ifndef 0-OBJECT_LIKE_MACRO_H
-#define 0-OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 /**
  *File: "0-object_like_macro.h"
  *Auth: "Mojolajah Adekunle"
  *Desc: "This is a header file that defines a macro named SIZE
  *       as an abbreviation for the token 1024
  */
-#include <stdlib.h>
-#include <stdio.h>
 #define SIZE 1024
 #endif
