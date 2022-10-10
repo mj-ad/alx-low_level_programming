9c605658cbbb0152cbd867edddb86f9d4d7c6601
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2022-10-10 06:05:01 -0700

    Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ *File: "1-pi.h"
+ *Auth: "Mojolajah Adekunle"
+ *Desc: "This is a header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359."
+ */
+
+#define PI 3.14159265359
+
+#endif
