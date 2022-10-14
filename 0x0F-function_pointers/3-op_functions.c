66e10bd3ed0683fe3f154dd52304a060631de7a2
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2022-10-14 03:58:51 -0700

    Operations file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ *op_add - sum of two integers
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: a+b
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *op_sub - sub integers
+ *@a: int 1
+ *@b: int 2
+ *
+ *Return: a-b
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ *op_mul - mul integers
+ *@a: int 1
+ *@b: int 2
+ *
+ *Return: a*b
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ *op_div - div integers
+ *@a: int 1
+ *@b: int 2
+ *
+ *Return: a/b
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ *op_mod - mod of integers
+ *@a: int 1
+ *@b: int 2
+ *
+ *Return: a%b
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
