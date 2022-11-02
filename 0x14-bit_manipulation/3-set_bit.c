827bb1c9db1cbcc97d8a81bbbd993b0b40c90ea6
mj-ad <adekunlemojolajah@gmail.com>
2022-11-02 04:03:49 -0700

    Set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 int b;
 if (index > (sizeof(unsigned long int) * 8))
 return (-1);
-b = (n >> index) & 1;
+b = (*n >> index) & 1;
 b = 1;
 return (1);
 }
