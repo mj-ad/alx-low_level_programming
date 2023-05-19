b2f4a47898115a49300e812e4039bd4a62b98e52
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-19 13:04:32 +0100

    Task 10 completed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,6 +6,35 @@
  */
 int main(void)
 {
-	
+	char i;
+	char c;
+	int j = 1;
+	int k = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		k = 0;
+		for (c = '0'; c <= '9'; c++)
+		{
+			if (k < j)
+			{
+				k++;
+				continue;
+			}
+			else
+			{
+				putchar(i);
+				putchar(c);
+				if (i != '8' || c != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+		}
+		j++;
+	}
+	putchar('\n');
 	return (0);
 }
