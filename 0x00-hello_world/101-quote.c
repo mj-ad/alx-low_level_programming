2658e4dfa3497c0ad3ae19e3ac53016b3e0b0bae
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-19 10:23:04 +0100

    Task 8 corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,";
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fwrite(str, sizeof(char), strlen(str), stderr);
 return (1);
 }
