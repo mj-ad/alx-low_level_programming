0bc48517d708357d5c706072ba292dec2238659d
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-19 10:18:20 +0100

    Task 8 completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 1
+ */
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,";
+fwrite(str, sizeof(char), strlen(str), stderr);
+return (1);
+}
