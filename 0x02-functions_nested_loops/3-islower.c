02297a9dae8bcf52f59e5c6123828f2241d2cf85
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-23 11:53:37 +0100

    Task 3 completed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
  * _islower - checks for lower case character
  * @c: letter to check
  *
- * Return: 0 if is lower and 1 otherwise
+ * Return: 1 if is lower and 0 otherwise
  */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (0);
+		return (1);
 	}
-	return (1);
+	return (0);
 }
