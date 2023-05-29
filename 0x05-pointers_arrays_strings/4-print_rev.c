23e572fd4867cc12555c59d602cb7d61339b75fe
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-29 15:12:04 +0100

    Task 4 completed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int size = 0;
+	int i;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+
+	for (i = size - 1; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
