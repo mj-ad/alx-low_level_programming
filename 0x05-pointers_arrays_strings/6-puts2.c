d7c32c09d10160955c3d2994f5af58ff2402f6ec
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-31 12:05:49 +0100

    Task 6 corrected

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,14 +11,13 @@ void puts2(char *str)
 	int size = 0;
 	int i;
 
-	while (str[size] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		size++;
-	}
-
-	for (i = 0; i > size - 1; i++)
-	{
-		if (i % 2 == 0)
+		if (i % 2 != 0)
+		{
+			continue;
+		}
+		else
 		{
 			_putchar(str[i]);
 		}
