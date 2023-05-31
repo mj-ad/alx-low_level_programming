d44c3e071b1171d6c3c8cf4ce72373005e333926
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-05-31 12:19:17 +0100

    Task 6 corrected

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - prits the second half of a string
+ * @str: string
+ *
+ * Return void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int n;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 != 0)
+	{
+		n = (len - 1) / 2;
+	}
+	else
+	{
+		n = len / 2;
+	}
+
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
