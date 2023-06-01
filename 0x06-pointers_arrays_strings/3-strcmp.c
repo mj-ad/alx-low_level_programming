0b74ff8cba2641301f083d95eb8c11ff456cb7b7
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-01 19:12:00 +0100

    Task 3 completed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,15 +13,15 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		if (s1[i] == s2[i] )
+		if (s1[i] == s2[i])
 			continue;
 		else
 			break;
 	}
 
-	if (s1[i] == s2[i])
+	if (*s1 == *s2)
 		return (0);
-	else if (s1[i] < s2[i])
+	else if (*s1 < *s2)
 		return (-15);
 	else
 		return (15);
