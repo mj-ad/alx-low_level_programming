82ab4ecbf3262c3ae0002c22e39fb468f6a26c69
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-01 19:09:10 +0100

    Task 3 completed

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: -15 , 15 or 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] == s2[i] )
+			continue;
+		else
+			break;
+	}
+
+	if (s1[i] == s2[i])
+		return (0);
+	else if (s1[i] < s2[i])
+		return (-15);
+	else
+		return (15);
+}
