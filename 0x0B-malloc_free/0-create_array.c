31e276b97f7e231823ce6085428c049979f3f5e1
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-13 15:40:36 +0100

    Task 0 handled malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,6 +20,8 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 		s = (char *)malloc(size * sizeof(char));
+		if (s == NULL)
+			return (NULL);
 		for (i = 0; i < size; i++)
 			s[i] = c;
 		return (s);
