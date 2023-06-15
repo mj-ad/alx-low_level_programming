25e6fcfb959fb4d8fd1f8ba0900cc7ab98caf68e
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-15 19:19:12 +0100

    Task 1 corrected

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 	while (s1[len1])
 		len1++;
-	s = malloc((sizeof(char) * (len1 + len2)) + 1);
+	if (n < len)
+		s = malloc((sizeof(char) * (len1 + n)) + 1);
+	else
+		s = malloc((sizeof(char) * (len1 + len2)) + 1);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
@@ -44,5 +47,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			s[i] = s2[j];
 		}
 	}
+	s[i++] = '\0';
 	return (s);
 }
