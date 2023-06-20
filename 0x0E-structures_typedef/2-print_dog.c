2e5f20876001affc69a575a25f02c04d47e20dcd
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-20 09:06:00 +0100

    Task 2 betty perfect

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,18 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if (d->name == NULL)
+		printf("Name: (nill)\n");
 	else
-	{
-		if (d->name == NULL)
-			printf("Name: (nill)\n");
-		else
-			printf("Name: %s\n", d->name);
+		printf("Name: %s\n", d->name);
 
-		if (d->age == '\0')
-			printf("Age: (nill)\n");
-		else
-			printf("Age: %f\n", d->age);
+	if (d->age == '\0')
+		printf("Age: (nill)\n");
+	else
+		printf("Age: %f\n", d->age);
 
-		if (d->owner == NULL)
-			printf("Owner: (nill)\n");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
+	if (d->owner == NULL)
+		printf("Owner: (nill)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
