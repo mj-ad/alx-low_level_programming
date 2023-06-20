0060b9a10bd0f5f767b29b6a541a7d8858831351
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-06-20 09:30:00 +0100

    Task 4 completed

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * dog_t - typedef
+ * new_dog - new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: new_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
