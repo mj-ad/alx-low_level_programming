d6f4eb82aad2c9d0fe9e030cd1c618e0a660e56e
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-08-18 15:31:41 +0100

    modified:   0x17-doubly_linked_lists/0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,19 +3,19 @@
 /**
  * print_dlistint - prints all elements of dlistint_t
  * @h: node
- * 
+ *
  * Return: number of nodes
-*/
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t counter = 0;
+        size_t counter = 0;
 
-    while(h != NULL)
-    {
-        printf("%d\n", h->n);
-        counter++;
-        h = h->next;
-    }
-    return (counter);
+        while (h != NULL)
+        {
+            printf("%d\n", h->n);
+            counter++;
+            h = h->next;
+        }
+        return (counter);
 }
