10fc5b47cec4825570b14d11467efc50fb0b16e7
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-08-18 16:32:46 +0100

    modified:   0x17-doubly_linked_lists/2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t node = (dlistint_t*)malloc(sizeof(dlistint_t));
+	dlistint_t *node = (dlistint_t*)malloc(sizeof(dlistint_t));
 
 	node->n = n;
 	node->next = (*head);
